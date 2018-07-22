645b54a6bb42d8f0dd62e02d16256202e0ba8096
Satoshi Watanabe <sassy.watanabe@gmail.com>
2018-07-22 15:13:22 +0900

    add base server.

---
+++ index.js
@@ -0,0 +1,34 @@
+const clova = require('@line/clova-cek-sdk-nodejs');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const clovaSkillHandler = clova.Client
+    .configureSkill()
+    .onLaunchRequest(responseHelper => {
+        responseHelper.setSimpleSpeech({
+            lang: 'ja',
+            type: 'PlainText',
+            value: 'ようこそ'
+        });
+    })
+    .onIntentRequest(async responseHelper => {
+        const intent = responseHelper.getIntent();
+        switch (intent) {
+            case 'MemberIntent':
+                responseHelper.setSimpleSpeech({
+                    lang: 'ja',
+                    type: 'PlainText',
+                    value: 'メンバーです'
+                })
+            break;
+        }
+    })
+    .onSessionEndedRequest(responseHelper => {
+    })
+    .handle();
+
+const app = new express();
+const clovaMiddleware = clova.Middleware({
+    applicationId: process.env.APPLICATION_ID
+});
+app.post('/clova', clovaMiddleware, clovaSkillHandler);
